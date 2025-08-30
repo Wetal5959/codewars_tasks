beaf92192cd3fdd3631ceea0fdb70300a541a5ba
Wetal5959 <wetal559@gmail.com>
2025-08-30 10:27:26 +0300

    testing1_2_3

---
+++ kyu_7/testing1_2_3.js
@@ -0,0 +1,50 @@
+
+
+
+let number = function(array){
+    const arrayNew = []
+   if(array.length !== 0) {array.forEach(function (element, index){
+       arrayNew.push(`${index +1}: ${element}`)
+    })}
+   return arrayNew;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let number = function(array) {
+    if (Array.isArray(array) && array.length !== 0) {
+        array.map
+    }
+}
+
