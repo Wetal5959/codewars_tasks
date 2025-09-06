28033443a74dbdc9b625b7181c155a4f86ab658d
Wetal5959 <wetal559@gmail.com>
2025-09-06 10:52:11 +0300

    sSplitStrings

---
+++ kyu_6/sSplitStrings.js
@@ -0,0 +1,13 @@
+function solution(str) {
+      const arr = []
+       if(str.length % 2) {
+           str = str + '_'
+       }
+    for (let i = 0; i < str.length; i += 2) {
+    arr.push(str[i] + str[i+1])
+    }
+    return arr
+}
+
+
+console.log(solution('m'))
