eba299e03a51bec7ab069313781a1cba47fb7ce8
Wetal5959 <wetal559@gmail.com>
2025-09-07 22:03:03 +0300

    changableDiagonal

---
+++ kyu_7/changableDiagonal.js
@@ -0,0 +1,26 @@
+function matrixDiagonal(matrix, value) {
+
+    if (matrix.length < Math.abs(value) + 1) {
+        return 0
+    }
+    let sum = 0
+    let leng = matrix.length
+    if (value > 0) {
+        for (let i = 0; i < leng - value; i++) {
+            sum += matrix[i + value][i]
+        }
+        return sum
+    }
+    for (let i = 0; i < leng + value; i++) {
+        sum += matrix[i][i - value]
+    }
+    return sum
+}
+
+matrix = [[1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]]
+
+console.log(matrixDiagonal(matrix, -3))
+
