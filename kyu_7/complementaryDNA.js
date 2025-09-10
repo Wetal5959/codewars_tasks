df875648e633c4c8e152f22838541d33687288db
Wetal5959 <wetal559@gmail.com>
2025-09-10 09:47:38 +0300

    complementaryDNA.js

---
+++ kyu_7/complementaryDNA.js
@@ -0,0 +1,20 @@
+function dnaStrand(dna) {
+    let dNA = 0;
+    return [...dna].reduce((acc, el) => {
+        if (el === 'A') {
+            return acc += 'T'
+        }
+        if (el === 'T') {
+            return acc += 'A'
+        }
+        if (el === 'C') {
+            return acc += 'G'
+        }
+        if (el === 'G') {
+            return acc += 'C'
+        }
+
+    }, '')
+
+}
+
